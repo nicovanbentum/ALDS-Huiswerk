8ee1330ec5f31b9e65a0db10ae60235299b67b08
nicovanbentum <niico0708@gmail.com>
2018-11-26 11:55:19 +0100

    opdracht 5 verbeterd

--- Week 2/Week2opdracht5.py
+++ Week 2/Week2opdracht5.py
@@ -18,7 +18,7 @@ def qsort(a, low=0,high=-1):
                 cmp_count = cmp_count + 1
                 m += 1
                 swap(a,m,j)
-                swap(a,low,m)
+        swap(a,low,m)
 
         if m > 0:
             qsort(a,low,m-1)
@@ -26,14 +26,17 @@ def qsort(a, low=0,high=-1):
 
 #################################################
 test_list = []
-for _ in range(20):
+for _ in range(10000):
     random_integer = random.randint(1, 10000)
     test_list.append(random_integer)
 
+cmp_count = 0
 qsort(test_list)
 print("number of comparisons: " + str(cmp_count))
 if sorted(test_list) != test_list:
     print("The quick sort implementation given in the assignment/reader doesn't actually work..")
+else:
+    print("list is sorted.")
 #################################################
 
 def qsort_worst_pivot(a, low=0,high=-1):
@@ -42,14 +45,14 @@ def qsort_worst_pivot(a, low=0,high=-1):
     if high == -1:
         high = len(a) -1
     if low < high:
-        swap(a, low, low) #Basically has no effect but instead of choosing a random index as pivot, it just uses the lowest
+        swap(a, low, high)
         m = low
         for j in range(low+1, high+1):
             if a[j] < a[low]:
                 cmp_count = cmp_count + 1
                 m += 1
                 swap(a,m,j)
-                swap(a,low,m)
+        swap(a,low,m)
 
         if m > 0:
             qsort_worst_pivot(a,low,m-1)
@@ -61,7 +64,11 @@ for _ in range(10000):
     random_integer = random.randint(1, 10000)
     test_list2.append(random_integer)
 
-qsort_worst_pivot(test_list)
-print("number of comparisons (worst pivot): " + str(cmp_count))
+cmp_count = 0
+qsort_worst_pivot(test_list2)
+print("number of comparisons: " + str(cmp_count))
+if sorted(test_list) != test_list:
+    print("The quick sort implementation given in the assignment/reader doesn't actually work..")
+else:
+    print("list is sorted.")
 #################################################
-
