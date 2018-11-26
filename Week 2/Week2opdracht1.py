c136a082ee88301f00fb51594a24b955089fc369
nicovanbentum <niico0708@gmail.com>
2018-11-26 22:45:39 +0100

    test voor 10000 toegevoegd

--- Week 2/Week2opdracht1.py
+++ Week 2/Week2opdracht1.py
@@ -1,21 +1,27 @@
 def machtv3(a, n):
     assert n > 0
 
-    m = 0
-    an = a
+    m = 1
+    multiplication_count = 0
+
     while n > 0:
         if n%2 == 0:
-            n = n // 2
-            an = (a*a)*n
-            m += 1
-
+            a = a*a
+            n = n//2
+            multiplication_count += 1
         else:
-            n = n - 1
-            an = a * (a*a)
-            m += 1            
+            m = m * a
+            n -= 1
+            multiplication_count += 1           
 
-    print("calculated result: " + str(an))
-    return m
+    print("calculated result: " + str(m))
+    return multiplication_count
 
 print("expected result: " + str(2**3))
 print("number of calculations: " + str(machtv3(2,3)))
+
+print("expected result: " + str(2**8))
+print("number of calculations: " + str(machtv3(2,8)))
+
+print("expected result: " + str(2**10000))
+print("number of calculations: " + str(machtv3(2,10000)))
